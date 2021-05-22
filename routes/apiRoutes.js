70e9bb2f84635d4d37a5adf7dca04d9f849eb049
Jvala2 <joevalancy@gmail.com>
2021-05-22 18:44:22 -0400

    undefined path

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,13 +1,13 @@
 
 
 const noteData = require('../db/db.json');
-
+const fs = require('fs');
 
 module.exports = (app) => {
 
 
   app.get('/api/notes', (req, res) => {
-    readFileAsync(path.join(__dirname, "./db/db.json"), "utf8")
+    fs.readFileAsync(path.join(__dirname, "./db/db.json"), "utf8")
     .then((data)=> {
         return res.json(JSON.parse(data));
     });
