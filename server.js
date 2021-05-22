0352cbd9209a092de6afa86d2a4111a2f96ba83c
Jvala2 <joevalancy@gmail.com>
2021-05-22 17:57:37 -0400

    fixed Server.js

--- server.js
+++ server.js
@@ -9,8 +9,10 @@ const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
-
+app.use(express.static("public"))
+app.get("/ping",(req,res) => {
+  res.send("Hello.")
+})
 require('./routes/apiRoutes')(app);
 require('./routes/htmlRoutes')(app);
 
