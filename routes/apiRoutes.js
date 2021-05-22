7bf2efb01b7dae677334dd298581d35943343d6d
Jvala2 <joevalancy@gmail.com>
2021-05-22 19:49:08 -0400

    What is wrong with line 10 of api routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,10 +2,9 @@
 
 const noteData = require('../db/db.json');
 const fs = require('fs');
+const path = require('path');
 
 module.exports = (app) => {
-
-  
 
   app.get('/api/notes', (req, res) => {
     fs.readFileAsync(path.join(__dirname, "./db/db.json"), "utf8")
