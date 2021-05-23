bad53295cadf0dcfc3b25481ac27d5199cacb5f4
Jvala2 <joevalancy@gmail.com>
2021-05-23 00:21:34 -0400

    YES IT WORKS

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,4 +1,5 @@
-const uuidv1 = require('uuid/v1');
+const { v4: uuidv4 } = require('uuid');
+console.log(uuidv4());
 const noteData = require('../db/db.json');
 const fs = require('fs');
 const path = require('path');
@@ -6,25 +7,26 @@ const path = require('path');
 module.exports = (app) => {
 
 
-    fs.readFile("./db/db.json", "utf8",function (error, data) {
-      var notes=JSON.parse(data)
+    
       app.get('/api/notes', (req, res) => {
-        res.json(notes)
-    })
+        fs.readFile("./db/db.json", "utf8",function (error, data) {
+          var notes=JSON.parse(data)
+        res.json(notes);
+    });
 });
 
 app.post('/api/notes', (req, res) => {
-  fs.readFile('db/db.json',(err, data) => {
-    if (err) throw err;
+  fs.readFile('db/db.json',(error, data) => {
+    if (error) throw error;
     let json = JSON.parse(data);
     let note = {
       title: req.body.title,
       text: req.body.text,
-      id: uuidv1()
+      id: uuidv4()
     }
     json.push(note);
-    fs.writeFile('db/db.json', JSON.stringify(json, null, 2), (err) => {
-      if (err) throw err;
+    fs.writeFile('db/db.json', JSON.stringify(json, null, 2), (error) => {
+      if (error) throw error;
       res.send('200');
     });
   });
@@ -33,9 +35,9 @@ app.post('/api/notes', (req, res) => {
 
   app.delete('/api/notes/:id', (req, res) => {
 
-    fs.readFile('db/db.json',(err, data) => {
+    fs.readFile('db/db.json',(error, data) => {
 
-      if (err) throw err;
+      if (error) throw error;
       let itemDelete = req.params.id;
       let json = JSON.parse(data);
       json.forEach((item, i) =>{
